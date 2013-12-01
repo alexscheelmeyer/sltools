0e9cba8b0f8709b3b1c3647af90541fdb5442802
Alex Scheel Meyer <alexscheelmeyer@gmail.com>
2013-12-01 15:17:18 +0300

    first version

---
+++ sltools.js
@@ -0,0 +1,24 @@
+
+
+exports.filterPage=function(items,page,pageSize){
+	items=items.filter(function(elem,index){
+        if((index>=(page*pageSize)) && (index<(page*pageSize+pageSize)))
+            return true;
+        else
+            return false;
+    });
+	return items;
+}
+
+exports.getPagination=function(numItems,pageIndex,pageSize){
+    var numPages=Math.ceil(numItems/pageSize);
+    var maxPages=5;
+    var pagination=[];
+    for(var p=pageIndex-1;p<numPages;p++){
+        if(pagination.length>=maxPages)break;
+        if(p>=0 && p<numPages){
+            pagination.push(p+1);
+        }
+    }
+    return pagination;
+}
